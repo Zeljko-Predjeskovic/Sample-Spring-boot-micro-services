a92915efdb83674711fdc0261346f5260f163221
Zeljko Predjeskovic <zepr2022000@gmail.com>
2021-10-06 18:23:35 +0200

    added music manager project

---
+++ Predjeskovic_Microservice_MusicManager/src/main/java/spg/pre/microservices/musicmanager/PredjeskovicMicroserviceMusicManagerApplication.java
@@ -0,0 +1,13 @@
+package spg.pre.microservices.musicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredjeskovicMicroserviceMusicManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PredjeskovicMicroserviceMusicManagerApplication.class, args);
+    }
+
+}
---
+++ Predjeskovic_Microservice_MusicManager/src/test/java/spg/pre/microservices/musicmanager/PredjeskovicMicroserviceMusicManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package spg.pre.microservices.musicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredjeskovicMicroserviceMusicManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
